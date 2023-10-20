90ecc5742c7931ca034fdaf2389ca359d90918d7
Napoleao1 <ernaninandoxd@gmail.com>
2023-10-20 16:34:16 -0300

    first commit

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+//import { StatusCodes } from 'http-status-codes';
+import *  as yup from 'yup';
+
+interface ICidade{
+  nome: string;
+
+}
+
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
+
+export const create =  async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+
+  try {
+    await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors:{
+        default: yupError.message,
+      }
+    })
+  }
+  
+
+  console.log(validateData);
+
+
+  return res.send('Create!');
+};
