fe63f8af58df76ffb4b0e0651d6e9ff3445fe214
Napoleao1 <ernaninandoxd@gmail.com>
2023-10-20 17:41:24 -0300

    validação de erros

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,29 +1,34 @@
 import { Request, Response } from 'express';
-//import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from 'http-status-codes';
 import *  as yup from 'yup';
 
 interface ICidade{
   nome: string;
+  estado: string;
 
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3)
 });
 
 export const create =  async (req: Request<{}, {}, ICidade>, res: Response) => {
-  let validateData: ICidade | undefined = undefined;
+  const validateData: ICidade | undefined = undefined;
 
   try {
-    await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    await bodyValidation.validate(req.body, { abortEarly: false });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors:{
-        default: yupError.message,
-      }
-    })
+
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_GATEWAY).json({errors}); 
   }
   
 
